6abd79c94b01a524f775b4dcc0e8c5db3cecfa76
Brian Boyle <brian@soundwave.com>
2015-07-02 15:12:36 +0100

    Initial Commit

---
+++ ShineExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShineExample
+//
+//  Created by Brian Boyle on 02/07/2015.
+//  Copyright (c) 2015 Soundwave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
